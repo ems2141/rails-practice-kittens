e84d6413901f5cf121e9949544b5a29a6ea90b5d
Ellie Schneiders <ems2141@gmail.com>
2014-07-21 16:32:40 -0600

    category must be present

--- app/models/categorization.rb
+++ app/models/categorization.rb
@@ -1,3 +1,5 @@
 class Categorization < ActiveRecord::Base
   belongs_to :category
+
+  validates :category, presence: true #uniqueness: {scope: :kitten_id}
 end
---
+++ spec/models/categorization_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+feature 'Categorizations' do
+  scenario 'Categorization cannot be blank' do
+    categorization = Categorization.new(
+        category: Category.new,
+    )
+    expect(categorization).to have(0).errors_on(:category)
+
+    categorization.category = nil
+    expect(categorization).to have(1).errors_on(:category)
+  end
+
+  
+end
